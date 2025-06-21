c958e180184557201e0b7292aa0d6b574236413c
river2056 <chen0625tung@gmail.com>
2025-06-22 07:53:54 +0800

    1.0.2
    + remove buttons and use actions instead

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from textual.app import App
-from textual import on
-from textual.containers import Container, HorizontalGroup, VerticalScroll
-from textual.widgets import Button, Checkbox, Digits, Header, Footer, Input
+from textual import on, events
+from textual.containers import Container, VerticalScroll
+from textual.widgets import Checkbox, Digits, Header, Footer, Input
 
 from models.models import TaskManager
 
@@ -11,13 +11,16 @@ class TodoApp(App):
 
     CSS_PATH = "styles.tcss"
 
-    BINDINGS = [("q", "quit", "Quit")]
+    BINDINGS = [
+        ("q", "quit", "Quit"),
+        ("a", "add", "Add new task"),
+        ("d", "delete", "Delete completed tasks"),
+    ]
 
     command = ""
     content = ""
     task_manager = TaskManager()
 
-    btns = None
     input_box = Input(placeholder="stuff you want to do", id="input_box")
     tasks = task_manager.get()
 
@@ -26,9 +29,6 @@ class TodoApp(App):
         with Container(classes="clock"):
             yield Digits("")
 
-        with HorizontalGroup(id="btns"):
-            yield Button(id="add", label="add")
-            yield Button(id="del", label="del")
         yield self.input_box
         with VerticalScroll(id="todos"):
             for task in self.tasks:
@@ -39,28 +39,15 @@ class TodoApp(App):
     def on_ready(self):
         self.update_clock()
         self.set_interval(1, self.update_clock)
+        self.query_one("#input_box").add_class("hide")
 
     def update_clock(self):
         now = datetime.now().time()
         self.query_one(Digits).update(f"{now:%T}")
 
-    def on_mount(self):
-        input_box = self.query_one("#input_box")
-        input_box.add_class("hide")
-        self.btns = self.query_one("#btns", HorizontalGroup)
-
-    @on(Button.Pressed)
-    async def handle_btn_pressed(self, event: Button.Pressed):
-        btn_id = event.button.id
-        if self.input_box.has_class("hide"):
-            match btn_id:
-                case "add":
-                    self.command = "add"
-                    self.toggle()
-                    self.input_box.focus()
-                case "del":
-                    self.command = "del"
-                    await self.handle_command()
+    def on_key(self, event: events.Key):
+        if event.key == "escape" and not self.input_box.has_class("hide"):
+            self.toggle()
 
     @on(Input.Submitted, "#input_box")
     async def on_enter_content(self, event: Input.Submitted):
@@ -76,6 +63,17 @@ class TodoApp(App):
         task_id = event.checkbox.id
         done = event.value
         self.task_manager.mark_as_done(task_id, done)
+
+    def action_add(self):
+        self.command = "add"
+        self.toggle()
+        self.input_box.focus()
+
+    async def action_delete(self):
+        for done_task in filter(lambda x: x.done, self.tasks):
+            self.task_manager.delete(done_task.id)
+        self.tasks = self.task_manager.get()
+        await self.refresh_tasks()
 
     async def handle_command(self):
         match (self.command):
@@ -97,12 +95,10 @@ class TodoApp(App):
             container.mount(check)
 
     def toggle(self):
-        if self.btns.has_class("hide"):  # type: ignore
-            self.btns.remove_class("hide")  # type: ignore
-            self.input_box.add_class("hide")
-        else:
-            self.btns.add_class("hide")  # type: ignore
+        if self.input_box.has_class("hide"):
             self.input_box.remove_class("hide")
+        else:
+            self.input_box.add_class("hide")
 
 
 if __name__ == "__main__":
